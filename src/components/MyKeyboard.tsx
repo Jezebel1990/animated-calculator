6e33dd2e864cff61586c1095fa6de0192c4f4b9d
Jezebel1990 <jezebelguedes@gmail.com>
2023-04-19 20:19:45 -0300

    create keyboard with number and operation

---
+++ src/components/MyKeyboard.tsx
@@ -0,0 +1,36 @@
+import * as React from "react";
+import Button from "./Button";
+import { View, Text } from "react-native";
+import { Styles } from "../styles/GlobalStyles";
+import {colors} from "../styles/Colors";
+
+export default function MyKeyboard() {
+    const [firstNumber, setFirstNumber] = React.useState("");
+    const [secondNumber, setSecondNumber] = React.useState("");
+    const [operation, setOperation] = React.useState("");
+    const [result, setResult] = React.useState<Number | null >(null);
+
+    const handleNumberPress = (buttonValue: string) => {
+        if (firstNumber.length < 10) {
+            setFirstNumber(firstNumber + buttonValue);
+        }
+    };
+   const handleOperationPress = (buttonValue: string) => {
+    setOperation(buttonValue);
+    setSecondNumber(firstNumber);
+    setFirstNumber("");
+   };
+const clear = () => {
+    setFirstNumber("");
+    setSecondNumber("");
+    setOperation("");
+    setResult(null);
+};
+
+    return(
+        <View>
+            
+        </View>
+
+    )
+}
