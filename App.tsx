c78d5913c7d6a6dee52d635138d776d062dfe08d
Jezebel1990 <jezebelguedes@gmail.com>
2023-04-19 19:52:24 -0300

    create an button

--- App.tsx
+++ App.tsx
@@ -3,27 +3,31 @@ import { StatusBar } from 'expo-status-bar';
 
 import React from 'react';
 import { ThemeContext } from "./src/context/ThemeContext";
-import { StyleSheet, Switch, Text, View } from 'react-native';
+import { StyleSheet, Switch, View } from 'react-native';
 import { useState } from 'react';
+import { colors } from './src/styles/Colors';
+import Button from './src/components/Button';
 
 export default function App() {
   const [theme, setTheme] = useState('light');
   return (
-   <View style={styles.container}>
-   <Text>Calculadora</Text>
+    <ThemeContext.Provider value={theme}>
+   <View style={theme === 'light'? styles.container : [styles.container, {backgroundColor: 'black'}]}>
    <StatusBar style="auto" />
    <Switch
     value={theme === 'light'}
     onValueChange={() => setTheme(theme === 'light'? 'dark': 'light')}
     />
+    <Button isBlue title='1' onPress={() =>       {alert('hello')} } />
    </View>
+   </ThemeContext.Provider>
   );
   }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: colors.light,
     alignItems: 'center',
     justifyContent: 'center',
   },
