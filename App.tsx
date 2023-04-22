4d65aa08cce97a52dee7f1224bfe112790c97ad9
Jezebel1990 <jezebelguedes@gmail.com>
2023-04-21 23:40:21 -0300

    add lottie in splash

--- App.tsx
+++ App.tsx
@@ -1,14 +1,24 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import React from 'react';
 import { ThemeContext } from "./src/context/ThemeContext";
 import { SafeAreaView, StatusBar, StyleSheet, Switch} from 'react-native';
 import { useState } from 'react';
 import { colors } from './src/styles/Colors';
 import MyKeyboard from './src/components/MyKeyboard';
+import SplashScreen from './src/screens/Splash';
+
+const Stack = createNativeStackNavigator()
 
 export default function App() {
  
   const [theme, setTheme] = useState('light');
   return (
+
+    <NavigationContainer> 
+      <Stack.Navigator initialRouteName="Splash" screenOptions={{headerShown: false}}>
+      <Stack.Screen name="Splash" component={SplashScreen} />
+
     <ThemeContext.Provider value={theme}>
    <SafeAreaView style={theme === 'light'? styles.container : [styles.container, {backgroundColor: 'black'}]}>
    <StatusBar style ="auto" />
@@ -16,10 +26,11 @@ export default function App() {
       value={theme === 'light'}
     onValueChange={() => setTheme(theme === 'light'? 'dark': 'light')}
     />
-   
-    <MyKeyboard />
+    <Stack.Screen name="MyKeyboard" component={MyKeyboard} /> 
    </SafeAreaView>
-   </ThemeContext.Provider>
+   </ThemeContext.Provider> 
+   </Stack.Navigator>
+   </NavigationContainer>
   );
   }
 
--- src/screens/Splash.tsx
+++ src/screens/Splash.tsx
@@ -1,19 +1,25 @@
-import React from 'react'
-import {StyleSheet ,View, Text } from 'react-native'
+import React from 'react';
+import {StyleSheet ,View } from 'react-native';
+import LottieView from 'lottie-react-native';
+import { colors } from '../styles/Colors';
+import calculatorJson from '../../assets/calculator.json';
 
 export interface SplashScreenProps {
 
 }
 const SplashScreen = () => {
     return (
-        <View>
-<Text>SplashScreen</Text>
+        <View style={styles.container}>
+            <LottieView source={calculatorJson} autoPlay loop resizeMode='contain' />
         </View>
     )
 }
     const styles = StyleSheet.create({
         container: {
             flex: 1,
+            backgroundColor: colors.light,
+            alignItems: 'center',
+            justifyContent: 'flex-start',
         },
     })
 export default SplashScreen
